909d49bcfbd766f7172310eed6dd407928c827d6
Jairo Alberto <jairo.albertto@gmail.com>
2025-04-27 15:40:34 -0300

    GitHub and Intellij connection test

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,8 @@ import boardgame.Position;
 public class Program {
     public static void main(String[] args) {
 
+        System.out.println("Github and Intellij connection test");
+
         Position pos = new Position(3, 5);
         System.out.println(pos);
     }
